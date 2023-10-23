9d309eae3e3264e0ce73bf19fd6cc0949842d215
Justas <justas.sobutas@gmail.com>
2023-10-23 09:47:01 +0300

    Remove unused button

--- fe-app/src/features/cabins/AddCabin.jsx
+++ fe-app/src/features/cabins/AddCabin.jsx
@@ -1,6 +1,5 @@
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
-import CabinTable from "./CabinTable";
 import CreateCabinForm from "./CreateCabinForm";
 
 const AddCabin = () => {
@@ -12,13 +11,6 @@ const AddCabin = () => {
         </Modal.Open>
         <Modal.Window name="cabin-form">
           <CreateCabinForm />
-        </Modal.Window>
-
-        <Modal.Open opens="table-form">
-          <Button>Show table</Button>
-        </Modal.Open>
-        <Modal.Window name="table-form">
-          <CabinTable />
         </Modal.Window>
       </Modal>
     </div>
