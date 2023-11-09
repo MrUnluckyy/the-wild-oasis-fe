9b74fdce1755be523d58874e6255aa3b04d90d6f
Justas <justas.sobutas@gmail.com>
2023-11-09 23:29:45 +0200

    Change eslint config

--- src/features/dashboard/Stats.jsx
+++ src/features/dashboard/Stats.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Stat from "./Stat";
 import {
   HiOutlineBanknotes,
--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ButtonIcon from "./ButtonIcon";
 import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
 import { useDarkMode } from "../context/DarkModeContext";
